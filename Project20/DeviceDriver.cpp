c8eec6f898fe92f115e146f010a3d82a2f16c0aa
hyunsung.jin <darkfishno1@gmail.com>
2023-06-05 15:58:21 +0900

    device driver read and write 기능 unit test 추가

--- Project20/DeviceDriver.cpp
+++ Project20/DeviceDriver.cpp
@@ -1,16 +1,36 @@
+#include <Windows.h>
+#include <exception>
 #include "DeviceDriver.h"
 
 DeviceDriver::DeviceDriver(FlashMemoryDevice* hardware) : m_hardware(hardware)
 {}
 
+
 int DeviceDriver::read(long address)
 {
     // TODO: implement this method properly
-    return (int)(m_hardware->read(address));
+
+    int firstReadTryValue = (int)(m_hardware->read(address));
+    for(int tryCount = 1; tryCount < TotalReadTryCount; tryCount++)
+    {
+        int nextReadTryValue = (int)(m_hardware->read(address));
+        if (firstReadTryValue == nextReadTryValue) continue;
+
+    	throw std::exception("Exception!!");
+        
+    }
+
+    return firstReadTryValue;
+
 }
 
 void DeviceDriver::write(long address, int data)
 {
     // TODO: implement this method
+
+    int readValue = (int)(m_hardware->read(address));
+    if (readValue != 0xFF)
+        throw std::exception("WriteFailException");
+
     m_hardware->write(address, (unsigned char)data);
 }
--- Project20/main.cpp
+++ Project20/main.cpp
@@ -1,4 +1,10 @@
+#include <Windows.h>
+#include <stdio.h>
 int main(int argc, char* argv[])
 {
-	
+	for(int i = 0; i < 30; i++)
+	{
+		printf("#");
+		Sleep(500);
+	}
 }
